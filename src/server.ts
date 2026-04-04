7a9e8e246d2e08dc643e117e40f11e1db7ab873d
Luke Skywalker <luke.skywalker31@outlook.com>
2026-04-04 00:45:14 -0300

    dynamic port

--- src/server.ts
+++ src/server.ts
@@ -24,7 +24,7 @@ app.get("/", (req, res) => {
   res.send("🚀 Backend ObservaCidade rodando");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 connectDatabase().then(() => {
   app.listen(PORT, () => {
